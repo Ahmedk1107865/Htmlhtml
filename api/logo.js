2d15b46bf614136d1d1f643767c45c8bfd25d7ca
CHUZA 🐥 <updatep696@gmail.com>
2025-06-09 12:37:40 +0500

    Update logo.js

--- api/logo.js
+++ api/logo.js
@@ -1,17 +1,17 @@
+// api/logo.js
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const logoType = req.query.logoType;
-  const logoText = req.query.logoText;
-  const apiUrl = `http:                                                                     
-
   try {
+    const logoType = req.query.logoType;
+    const logoText = req.query.logoText;
+    const apiUrl = `http:                                                                     
+
     const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
     const logoBuffer = Buffer.from(response.data, 'binary');
     res.setHeader('Content-Type', 'image/png');
     res.setHeader('Content-Disposition', `//fi3.bot-hosting.net:21943/api/ephoto/${logoType}?text=${logoText}`;
 
-  try {
     const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
     const logoBuffer = Buffer.from(response.data, 'binary');
     res.setHeader('Content-Type', 'image/png');
